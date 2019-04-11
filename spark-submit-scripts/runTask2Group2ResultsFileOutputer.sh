da765e454de1d5932a4421ba85a6aeece0c51748
markcberman <markcberman@yahoo.com>
2019-04-11 04:07:35 -0500

    spark yarn client submit scripts

---
+++ spark-submit-scripts/runTask2Group2ResultsFileOutputer.sh
@@ -0,0 +1,3 @@
+/usr/hdp/current/spark2-client/bin/spark-submit --files ./log4j.properties,applicationRemote.conf --driver-java-options "-Dlog4j.configuration=./log4j.properties" --conf "spark.executor.extraJavaOptions='-Dlog4j.configuration=./log4j.properties';spark.executor.logs.rolling.strategy=size;spark.executor.logs.rolling.maxSize=10485760;spark.executor.logs.rolling.maxRetainedFiles=100;spark.streaming.stopGracefullyOnShutdown=true"  --master yarn --deploy-mode client   --driver-memory 32g  --conf spark.driver.maxResultSize=6g   --executor-memory 8g --num-executors 4  --executor-cores 12 --class cs598ccc.task2.Group2ResultsFileOutputer  --driver-class-path ~ --verbose  --name task2_setup cs598ccctask2.jar applicationRemote.conf
+
+
